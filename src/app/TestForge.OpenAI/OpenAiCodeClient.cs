3183a4c7dbdf072a5a60c82271885dd231e7bd58
DJPokis <DJPokis@gmail.com>
2023-02-12 21:54:36 +0200

    add placeholder for open api client calls

--- src/app/TestForge.OpenAI/OpenAiCodeClient.cs
+++ src/app/TestForge.OpenAI/OpenAiCodeClient.cs
@@ -1,12 +1,28 @@
-﻿namespace TestForge.OpenAI
+﻿using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using TestForge.ServiceCollection;
+
+namespace TestForge.OpenAI
 {
     public class OpenAiCodeClient
     {
         private readonly OpenAiSettings _settings;
+        private readonly IOpenAIService _openAiService;
 
         public OpenAiCodeClient(OpenAiSettings settings)
         {
             _settings = settings;
+            _openAiService = SingletonServiceProvider.Instance.GetService<IOpenAIService>();
+            _openAiService.SetDefaultModelId(Models.CodeDavinciV2);
+        }
+
+        public void Test()
+        {
+            _openAiService.Edit.CreateEdit(new EditCreateRequest()
+            {
+                
+            });
         }
     }
 }
---
+++ src/app/TestForge.ServiceCollection/SingletonServiceProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.GPT3.Extensions;
+
+namespace TestForge.ServiceCollection
+{
+    public class SingletonServiceProvider
+    {
+        private static SingletonServiceProvider _instance;
+        private static readonly object _lock = new object();
+        private ServiceProvider _serviceProvider;
+
+        private SingletonServiceProvider()
+        {
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public static SingletonServiceProvider Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SingletonServiceProvider();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public T GetService<T>()
+        {
+            return _serviceProvider.GetService<T>();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Add any required services to the collection here.
+            AddOpenApi(services);
+        }
+
+        private void AddOpenApi(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder().AddUserSecrets<SingletonServiceProvider>();
+            var configuration = builder.Build();
+
+            string apiKey = configuration["OpenAI:ApiKey"];
+
+            services.AddOpenAIService(settings =>
+            {
+                settings.ApiKey = apiKey;
+            });
+        }
+    }
+}
